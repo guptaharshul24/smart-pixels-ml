0afaca072ba72462590a8c742432af5ad05eb485
Arghya112358 <arghyaranjandas@gmail.com>
2025-06-25 00:57:38 +0200

    debug loss

--- losses/diag_loss_nll.py
+++ losses/diag_loss_nll.py
@@ -4,34 +4,16 @@ import tensorflow_probability as tfp
 # custom loss function foo diag model (with 8 outputs)
 def custom_diag_loss(y, p_base, ):
     mu = p_base[:, 0:4]
-    raw_diag = p_base[:, 4:8]
     
     minval=1e-9
     maxval=1e9
-    scale = 512
     
-    # creating each matrix element in 4x4
-    Mdia = tf.clip_by_value(raw_diag, 
-                            clip_value_min=minval,
-                            clip_value_max=maxval
-                            )
+    sigma_diag = tf.clip_by_value(p_base[:, 4:8], minval, maxval)
     
-    
-    # placeholder zero element
-    zeros = tf.zeros_like(Mdia[:,0])
-    
-    # assembles scale_tril matrix
-    row1 = tf.stack([Mdia[:,0],zeros,zeros,zeros])
-    row2 = tf.stack([zeros,Mdia[:,1],zeros,zeros])
-    row3 = tf.stack([zeros,zeros,Mdia[:,2],zeros])
-    row4 = tf.stack([zeros,zeros,zeros,Mdia[:,3]])
-
-    scale_tril = tf.transpose(tf.stack([row1,row2,row3,row4]),perm=[2,0,1])
-
-    dist = tfp.distributions.MultivariateNormalTriL(
-        loc = mu, 
-        scale_tril = scale_tril
-    ) 
+    dist = tfp.distributions.MultivariateNormalDiag(
+        loc=mu,
+        scale_diag=sigma_diag
+    )
     NLL = -dist.log_prob(y)
 
     return tf.reduce_sum(NLL) 
